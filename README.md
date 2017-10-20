0651261a231d55d0e67e0baf757c28b752b56ddd
Nena Furtmeier <Nena@Furtmeier.IT>
2017-10-20 17:18:24 +0200

    added script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # phpIMAPArchiver
+One of my customers had the problem that her iPhone couldn't handle the huge amount of emails in her INBOX.
+
+So I wrote this script to regularly archive her INBOX to another folder.
+
+## Getting Started
+Just clone this repository and edit the file archive.php:
+
+ $server = "localhost:143";
+ $user = "test@test.de";
+ $password = "123456";
+
+ $from = "INBOX";
+ $to = "INBOX.Archiv.{Y}";
+
+ $olderThan = "-6 months"; //strtotime format
+
+Then run the script:
+
+ php archive.php
+
+## Cronjob
+
+To regularly archive the messages, add a cronjob:
+
+ 0 3 * * 0 /usr/bin/php /home/nemiah/archive.php
---
+++ archive.php
@@ -0,0 +1,75 @@
+<?php
+/*
+phpIMAPArchiver archives message from INBO to another mailbox
+Copyright (C) 2017  Nena Furtmeier
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//CHANGE SETTINGS HERE
+$server = "localhost:143";
+$user = "test@test.de";
+$password = "123456";
+
+$from = "INBOX";
+$to = "INBOX.Archiv.{Y}";
+
+$olderThan = "-6 months"; //strtotime format
+//END SETTINGS
+
+
+if(function_exists('date_default_timezone_set'))
+	date_default_timezone_set('Europe/Berlin');
+
+$server = "{".$server."/novalidate-cert}";
+$olderThan = strtotime($olderThan);
+$mbox = imap_open($server.$from, $user, $password);
+imap_expunge($mbox);
+
+$folders = imap_list($mbox, $server, "*");
+foreach($folders AS $k => $folder)
+	$folders[$k] = str_replace($server, "", $folder);
+
+$UIDs = imap_search($mbox, "BEFORE ".date("d-M-Y", $olderThan), SE_UID);
+
+foreach($UIDs AS $UID){
+	$header = imap_fetch_overview($mbox, $UID, FT_UID);
+	$datum = $header[0]->udate;
+	
+	$currentTo = preg_replace_callback("/\{(.*)\}/", function($t) use ($datum) { 
+		return date($t[1], $datum);
+	}, $to);
+	
+	echo date("d.m.Y H:i:s", $header[0]->udate);
+	
+	echo " -> $currentTo";
+	
+	if(!in_array($currentTo, $folders))
+		if(imap_createmailbox($mbox, imap_utf7_encode($server.$currentTo)))
+			$folders[] = $currentTo;
+	
+	if(!in_array($currentTo, $folders)){
+		echo " TARGET DOES NOT EXIST! \n";
+		continue;
+	}
+	
+	if(imap_mail_move($mbox, $UID, $currentTo, CP_UID))
+		echo " OK";
+	else
+		echo " FEHLER!";
+	
+	echo "\n";
+}
+
+imap_expunge($mbox);
